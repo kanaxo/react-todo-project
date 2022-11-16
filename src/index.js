60ea74c1f1131f7e673b29624881e728d6ebfeac
kanaxo <60850434+kanaxo@users.noreply.github.com>
2022-11-16 16:59:50 +0800

    Deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import About from "./functionBased/pages/About"
 import NotMatch from "./functionBased/pages/NotMatch"
 import Navbar from "./functionBased/components/Navbar"
@@ -16,14 +16,14 @@ import "./functionBased/App.css"
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <Router basename={process.env.PUBLIC_URL}>
             <Navbar/>
             <Routes>
                 <Route path="/" element={<TodoContainer/>}/>
                 <Route path="about/*" element={<About/>}/>
                 <Route path="*" element={<NotMatch/>}/>
             </Routes>
-        </BrowserRouter>
+        </Router>
         </React.StrictMode>, 
     document.getElementById("root"))
 
